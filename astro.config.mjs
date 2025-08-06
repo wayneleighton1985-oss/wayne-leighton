3fd27e1159f48df96fb3c18e0d6d4738a3a8f3b4
CLAYYO <uktinter@gmail.com>
2025-08-06 11:53:48 +0100

    Switch to implicit OAuth flow for static deployment

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,8 @@
 import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
-import cloudflare from '@astrojs/cloudflare';
 
 export default defineConfig({
   integrations: [tailwind()],
-  output: 'hybrid',
-  adapter: cloudflare(),
+  output: 'static',
   site: 'https://wayne-leighton.pages.dev'
 });
