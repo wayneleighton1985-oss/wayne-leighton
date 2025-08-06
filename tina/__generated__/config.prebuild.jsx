0d53d45c8267b6dc85678d39a7950988ed61b77a
CLAYYO <uktinter@gmail.com>
2025-08-06 12:23:34 +0100

    Add TinaCMS configuration and environment variables

---
+++ tina/__generated__/config.prebuild.jsx
@@ -0,0 +1,109 @@
+// tina/config.ts
+import { defineConfig } from "tinacms";
+var branch = process.env.CF_PAGES_BRANCH || process.env.GITHUB_BRANCH || process.env.VERCEL_GIT_COMMIT_REF || process.env.HEAD || "main";
+var config_default = defineConfig({
+  branch,
+  // Get this from tina.io
+  clientId: process.env.NEXT_PUBLIC_TINA_CLIENT_ID,
+  // Get this from tina.io
+  token: process.env.TINA_TOKEN,
+  build: {
+    outputFolder: "admin",
+    publicFolder: "public"
+  },
+  media: {
+    tina: {
+      mediaRoot: "images/books",
+      publicFolder: "public"
+    }
+  },
+  // See docs on content modeling for more info on how to setup new content models: https://tina.io/docs/schema/
+  schema: {
+    collections: [
+      {
+        name: "books",
+        label: "Books",
+        path: "src/content/books",
+        format: "md",
+        fields: [
+          {
+            type: "string",
+            name: "title",
+            label: "Title",
+            isTitle: true,
+            required: true
+          },
+          {
+            type: "string",
+            name: "author",
+            label: "Author",
+            required: true
+          },
+          {
+            type: "rich-text",
+            name: "description",
+            label: "Description",
+            required: true
+          },
+          {
+            type: "number",
+            name: "priceDigital",
+            label: "Digital Price",
+            required: true
+          },
+          {
+            type: "number",
+            name: "pricePrint",
+            label: "Print Price",
+            required: true
+          },
+          {
+            type: "image",
+            name: "coverImageUrl",
+            label: "Cover Image",
+            required: true
+          },
+          {
+            type: "string",
+            name: "category",
+            label: "Category",
+            options: [
+              "Leadership",
+              "Business",
+              "Marketing",
+              "Personal Development"
+            ],
+            required: true
+          },
+          {
+            type: "number",
+            name: "publishedYear",
+            label: "Published Year",
+            required: true
+          },
+          {
+            type: "number",
+            name: "pages",
+            label: "Pages",
+            required: true
+          },
+          {
+            type: "string",
+            name: "isbn",
+            label: "ISBN",
+            required: true
+          },
+          {
+            type: "rich-text",
+            name: "body",
+            label: "Body",
+            isBody: true
+          }
+        ]
+      }
+    ]
+  }
+});
+export {
+  config_default as default
+};
---
+++ tina/config.ts
@@ -0,0 +1,115 @@
+import { defineConfig } from "tinacms";
+
+// Your hosting provider likely exposes this as an environment variable
+const branch =
+  process.env.CF_PAGES_BRANCH ||
+  process.env.GITHUB_BRANCH ||
+  process.env.VERCEL_GIT_COMMIT_REF ||
+  process.env.HEAD ||
+  "main";
+
+export default defineConfig({
+  branch,
+
+  // Get this from tina.io
+  clientId: process.env.NEXT_PUBLIC_TINA_CLIENT_ID,
+  // Get this from tina.io
+  token: process.env.TINA_TOKEN,
+
+  build: {
+    outputFolder: "admin",
+    publicFolder: "public",
+  },
+  media: {
+    tina: {
+      mediaRoot: "images/books",
+      publicFolder: "public",
+    },
+  },
+  // See docs on content modeling for more info on how to setup new content models: https://tina.io/docs/schema/
+  schema: {
+    collections: [
+      {
+        name: "books",
+        label: "Books",
+        path: "src/content/books",
+        format: "md",
+        fields: [
+          {
+            type: "string",
+            name: "title",
+            label: "Title",
+            isTitle: true,
+            required: true,
+          },
+          {
+            type: "string",
+            name: "author",
+            label: "Author",
+            required: true,
+          },
+          {
+            type: "rich-text",
+            name: "description",
+            label: "Description",
+            required: true,
+          },
+          {
+            type: "number",
+            name: "priceDigital",
+            label: "Digital Price",
+            required: true,
+          },
+          {
+            type: "number",
+            name: "pricePrint",
+            label: "Print Price",
+            required: true,
+          },
+          {
+            type: "image",
+            name: "coverImageUrl",
+            label: "Cover Image",
+            required: true,
+          },
+          {
+            type: "string",
+            name: "category",
+            label: "Category",
+            options: [
+              "Leadership",
+              "Business", 
+              "Marketing",
+              "Personal Development"
+            ],
+            required: true,
+          },
+          {
+            type: "number",
+            name: "publishedYear",
+            label: "Published Year",
+            required: true,
+          },
+          {
+            type: "number",
+            name: "pages",
+            label: "Pages",
+            required: true,
+          },
+          {
+            type: "string",
+            name: "isbn",
+            label: "ISBN",
+            required: true,
+          },
+          {
+            type: "rich-text",
+            name: "body",
+            label: "Body",
+            isBody: true,
+          },
+        ],
+      },
+    ],
+  },
+});
