1c22c756a151ec3a240b2b68760a40b1a3cb2b92
wayneleighton1985-oss <wayne.leighton1985@gmail.com>
2025-08-14 19:03:54 +0100

    Add TinaCMS admin assets and update generated client for deployment

--- tina/__generated__/client.ts
+++ tina/__generated__/client.ts
@@ -1,5 +1,5 @@
 import { createClient } from "tinacms/dist/client";
 import { queries } from "./types";
-export const client = createClient({ cacheDir: '/Users/nickroberts/Documents/Wayne-Leighton-Books/tina/__generated__/.cache/1755189848180', url: 'https://content.tinajs.io/1.6/content/56844d01-3e31-4c3c-b165-956a1f1e0198/github/main', token: '7a0024bf17443a12d8bf5054e078bd42af60c5e0', queries,  });
+export const client = createClient({ cacheDir: '/Users/nickroberts/Documents/Wayne-Leighton-Books/tina/__generated__/.cache/1755194551277', url: 'https://content.tinajs.io/1.6/content/56844d01-3e31-4c3c-b165-956a1f1e0198/github/main', token: '7a0024bf17443a12d8bf5054e078bd42af60c5e0', queries,  });
 export default client;
   
